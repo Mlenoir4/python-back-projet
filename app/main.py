ddfa830403fd982bee8962d447ef34b5823aabcf
mlenoir4 <mlenoir4@myges.fr>
2023-05-17 12:12:08 +0200

    last rush

---
+++ app/main.py
@@ -0,0 +1,40 @@
+#System imports
+
+
+# From Librairy import the class
+from fastapi import FastAPI
+
+# From Local import the class
+from routers import userService, enterpriseService, planingService
+import auth
+
+app = FastAPI()
+
+
+app.include_router(enterpriseService.router, tags=["enterprises"])
+app.include_router(userService.router, tags=["users"])
+app.include_router(planingService.router, tags=["planings"])
+app.include_router(auth.router, tags=["auth"])
+
+
+custom_responses = {
+    204: {"description": "No content"},
+    400: {"description": "Bad request"},
+    401: {"description": "Unauthorized"},
+    403: {"description": "Forbidden"},
+    404: {"description": "Not found"},
+    406: {"description": "Not acceptable"},
+    500: {"description": "Internal server error"}
+}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/initDatabase", tags=["initDatabase"])
+async def initDatabase():
+    userService.users = userService.initUsers()
+    enterpriseService.enterprise = enterpriseService.initEnterprise()
+    planingService.planings = planingService.initPlanings()
+    return {"message": "Database initialized"}
